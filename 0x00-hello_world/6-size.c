101e9bb9573ad8924bb47805366c26f696add0f6
Hcc25 <hildacheptekei@outlook.com>
2023-02-16 09:11:29 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,6 @@ printf("Size of a char: %lu byte(s)\n", (unasigned long)sizeof(a));
 printf("Size of an int: %lu byte(s) \n", (unasigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s) \n", (unasigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s) \n", (unasigned long)sizeof(d));
-printf("Size of a float: %lu byte(s) \n", (unasigned long)sizeof(f));
+printf("Size of a float: %lu byte(s) \n", unasigned long)sizeof(f));
 return (0);
 }
