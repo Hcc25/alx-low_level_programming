7b133f90506d592a51d96416018be67e9e35cee8
Hcc25 <hildacheptekei@outlook.com>
2023-02-16 02:42:45 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main this prints "Programming is like building a multilingual puzzle
+ * return: this returns 0
+ * */
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle\n");
+		return (0);
+}
