989a939c5331a63f6a98149534f157beacaaee73
Hcc25 <hildacheptekei@outlook.com>
2023-02-16 07:52:15 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,5 +15,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(w));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
-return: (0);
+return (0);
 }
