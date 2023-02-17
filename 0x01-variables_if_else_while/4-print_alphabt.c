828b1f935ce9c85de3c091315ad1b23324f2b1c5
Hcc25 <hildacheptekei@outlook.com>
2023-02-17 10:00:43 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main -  prints the alphabet in lowercase, followed by a new line
+* Return: Always 0
+*/
+int main(void)
+{
+char la;
+for (la = 'a'; la <= 'z'; la++)
+{
+if (la != 'e' && la != 'q')
+putchar(la);
+}
+putchar('\n');
+return (0);
+}
