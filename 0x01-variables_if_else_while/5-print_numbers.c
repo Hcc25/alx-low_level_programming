5396ddf7d4b1355e3fbe9a0b7f1290060249d9c6
Hcc25 <hildacheptekei@outlook.com>
2023-02-21 00:17:32 -0800

    0-putchar.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,7 +7,7 @@ int main(void)
 {
 int num;
 for (num = 0; num < 10; num++)
-printf("number%d", num);
+printf("%d", num);
 printf("\n");
 return (0);
 }
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - a program that prints _putchar
+* Return: Always (0)
+*/
+int main(void)
+{
+	printf("_putchar");
+
+	printf("\n");
+
+	return (0);
+}
