baaff4e2898665f21701f534827613a2e7eae012
Hcc25 <hildacheptekei@outlook.com>
2023-02-21 07:40:47 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
-* main - print code 
+* main - print code
 * Return: Always (0)
 */
 void print_alphabet(void)
 {
 char ch;
-
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 _putchar(ch);
