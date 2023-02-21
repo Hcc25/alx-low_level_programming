f966c78c0263484f9351088988279de179e7b58b
Hcc25 <hildacheptekei@outlook.com>
2023-02-21 10:46:19 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ void jack_bauer(void)
 				_putchar('0');
 				_putchar(i + '0');
 			}
-			else if (i >= 10
+			else if (i >= 10)
 					{
 					_putchar((i / 10) + '0');
 					_putchar((i % 10) + '0');
@@ -26,12 +26,13 @@ void jack_bauer(void)
 				_putchar ('0');
 				_putchar (j + '0');
 
-				 else if (j > 10)
+				 else if (j >= 10)
 				 {
 				 _putchar(':');
-				 _putchar((j / 10) + '0')
-				 _putchar((j % 10) + '0')
+				 _putchar((j / 10) + '0');
+				 _putchar((j % 10) + '0');
 				 }
+				 _putchar('\n');
 
 	}
 }
