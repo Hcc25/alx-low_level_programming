3bd99d78b6429e8794544eaf1f49a83b7cbee0b6
Hcc25 <hildacheptekei@outlook.com>
2023-02-21 04:54:20 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,13 @@
  * main - a program that prints the alphabet in lowercase
  * Return: Always (0)
  */
-int main(void)
+void print_alphabet(void)
 {
-	int ch;
-	{
+	char ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
-putchar(ch);
-	}
-putchar("\n");
+_putchar(ch);
+	
+_putchar("\n");
 
 	return (0);
 }
