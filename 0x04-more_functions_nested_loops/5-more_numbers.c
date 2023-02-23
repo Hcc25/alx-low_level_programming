c35cdf12146de207bf78941005ca4e533c169a3e
Hcc25 <hildacheptekei@outlook.com>
2023-02-23 01:16:09 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,10 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; y < 15; y++)
 		{
-			_putchar(j + '0');
+			_putchar(y + '0');
 		}
 		_putchar('\n');
 	}
 }
-
