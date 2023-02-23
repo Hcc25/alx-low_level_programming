5df944d5e2479af618faa5ba49c71b3e1cf78381
Hcc25 <hildacheptekei@outlook.com>
2023-02-23 05:38:45 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * fizz_buzz - printing to 100
+ */
+void fizz_buzz(void)
+{
+	int i;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	printf("\n");
+	return (0);
+}
