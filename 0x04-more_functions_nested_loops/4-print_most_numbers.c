06b7cefe381042fe04d0c4bedbcd42de1886c929
Hcc25 <hildacheptekei@outlook.com>
2023-02-22 23:45:38 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - this prints most numbers
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; aaa < 10; a++)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
