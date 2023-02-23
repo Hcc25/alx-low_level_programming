aab741653d8c7994841e6f5dfa12ce4e7e200d9b
Hcc25 <hildacheptekei@outlook.com>
2023-02-23 03:21:34 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,11 +14,11 @@ void print_diagonal(int n)
 	}
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < i; j++)
+		for (int j = 0; j <= i; j++)
 		{
 			_putchar(' ');
 		}
-		_putchar('\\');
+		_putchar(92);
 		_putchar('\n');
 	}
 }
