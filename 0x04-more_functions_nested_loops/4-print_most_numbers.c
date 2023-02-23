3822cc71013cc495d083185cdb1a42b1465aa782
Hcc25 <hildacheptekei@outlook.com>
2023-02-23 01:27:42 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,11 @@ void print_most_numbers(void)
 {
 	int a;
 
-	for (a = 0; aaa < 10; a++)
+	for (a = 0; a < 10; a++)
 	{
 		if (a != 2 && a != 4)
 		{
-			_putchar(i + '0');
+			_putchar(a + '0');
 		}
 	}
 	_putchar('\n');
