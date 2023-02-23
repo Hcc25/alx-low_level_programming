ba0b3b814584e897ca577b811f452d71c5bd1b7c
Hcc25 <hildacheptekei@outlook.com>
2023-02-23 05:46:36 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,10 +5,14 @@
  */
 void fizz_buzz(void)
 {
-	int i;
+	int x;
 
 	for (x = 1; x <= 100; x++)
 	{
+		if (x == 100)
+		{
+			printf("Buzz");
+		}
 		if (x % 3 == 0 && x % 5 == 0)
 		{
 			printf("FizzBuzz ");
