222eeab170fd708d65dd2fae650109031a3dbfe9
Hcc25 <hildacheptekei@outlook.com>
2023-02-27 05:32:47 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts2 - prints every characterof a string
+ * puts2 - prints every character of a string
  * @str: a parameter
  */
 void puts2(char *str)
@@ -8,7 +8,7 @@ void puts2(char *str)
 	int len, i;
 
 	len = strlen(str);
-	for (int i = 0; i < len; i++)
+	for (i = 0; i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
