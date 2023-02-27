d87d7ba072f0459c3248f07a8122f46118418ea5
Hcc25 <hildacheptekei@outlook.com>
2023-02-27 03:55:15 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - printing a string in reverse
+ * @s: the string to be printed in reverse
+ */
+void print_rev(char *s)
+{
+int i, n;
+
+n = strlens(s);
+for (i = n - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+}
