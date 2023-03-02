4d2cfa06937249eb4204c48c95632be5d55ff3c0
Hcc25 <hildacheptekei@outlook.com>
2023-03-02 13:01:29 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,8 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int result;
+	int i;
 
-	strcmp(s1, s2);
-	return (result);
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
 }
