9c2ef8a3ea4df72e50f3979b920fcc6c4728f286
Hcc25 <hildacheptekei@outlook.com>
2023-03-02 11:17:45 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - converting any string to upper case
+ * @x: string parameter
+ * Return: string
+ */
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+			x[i] = x[i] - 32;
+		i++;
+	}
+	return (x);
+}
