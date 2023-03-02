ce4d4ddad81d380a9fe32714c2208b3e2a973d89
Hcc25 <hildacheptekei@outlook.com>
2023-03-02 10:02:46 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string>
+/**
+ * _strcmo - for comparing purpose
+ * @s1: parameter
+ * @s2: parameter
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+	return (dest);
+}
