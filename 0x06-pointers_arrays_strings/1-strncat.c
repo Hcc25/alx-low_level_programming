36151a90fd01f8467f14d7b2e9b30bf605afc46b
Hcc25 <hildacheptekei@outlook.com>
2023-03-02 09:08:14 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strncat - function to append some charx
+ * @dest: a parameter
+ * @src: a parameter
+ * @n: a parameter
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
