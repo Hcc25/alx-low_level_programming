147aebea5c5d6a1cd9afe88a74ddd43cfc6cae3a
Hcc25 <hildacheptekei@outlook.com>
2023-03-08 10:48:33 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - printing a string in reverse using recursion
+ * @s: string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	putchar(*s);
+}
