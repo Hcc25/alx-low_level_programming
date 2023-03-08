d8674d05be0316c3fe0d4c42bae2cc176334301a
Hcc25 <hildacheptekei@outlook.com>
2023-03-08 11:36:32 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+int _sqrt(int, int);
+/**
+ * _sqrt_recursion - sqrt using recursion
+ * @n: a parameter
+ * Return: square root of a number
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+/**
+ * _sqrt - recursive square root
+ * @n: number
+ * @i: iterator
+ * Return: a number
+ */
+int _sqrt(int n, int i)
+{
+	int square = i * i;
+
+	if (square > n)
+		return (-1);
+	if (square == n)
+		return (i);
+	return (sqrt(n, i + 1));
+}
