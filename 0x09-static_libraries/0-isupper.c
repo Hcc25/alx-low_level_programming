f5cbbfc424edbee50a48459f2f69433ea7d052b4
Hcc25 <hildacheptekei@outlook.com>
2023-03-10 04:50:28 -0800

    my tasks

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - upppercase letters
+ * @c: char to check
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
