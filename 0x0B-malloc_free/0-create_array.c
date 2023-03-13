49b172f2b88dcbe4c755e794c6a446a294d5f466
Hcc25 <hildacheptekei@outlook.com>
2023-03-13 11:51:36 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	array = (char *) malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++;)
+	for (i = 0; i < size; i++)
 		array[i] = c;
 	return (array);
 }
