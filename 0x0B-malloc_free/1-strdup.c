07c9e76a004ce155e2af16d2c88e307cde2af257
Hcc25 <hildacheptekei@outlook.com>
2023-03-14 10:18:40 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,18 +9,17 @@
 char *_strdup(char *str)
 {
 	char *aaa;
-	int i;
-	int r = 0;
+	int i, r = 0;
 
 	if (str == NULL)
 		return (NULL);
 	i = 0;
-	while (str[i] != '\0')
+		whil(str[i] != '\0')
 		i++;
-	aaa = malloc(sizeof(char) * (i + 1);
-			if (aaa == NULL)
-			return (NULL);
-			for (r = 0; str[r]; r++)
-			aaa[r] = str[r];
-			return (aaa);
-			}
+	aaa = malloc(sizeof(char) * (i = 1));
+	if (aaa == NULL)
+		return (NULL);
+	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];
+	return (aaa);
+}
