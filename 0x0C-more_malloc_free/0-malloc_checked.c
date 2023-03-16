f931e4c1c69b65c808726a7df7b3cbc3d06c3453
Hcc25 <hildacheptekei@outlook.com>
2023-03-16 10:00:23 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,15 @@
-#include <stdlib.h>
 #include "main.h"
 /**
- * *malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
- * Return: pointer to the allocated memory
+ * *malloc_checked - cause normal process termination with a status value of 98
+ * @b: allocated memory
+ * Return: a pointer to the allocated memory
  */
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *i;
 
-	ptr = malloc(b);
-	if (ptr == NULL)
+	i = malloc(b);
+	if (i == NULL)
 		exit(98);
-	return (ptr);
+	return (i);
 }
