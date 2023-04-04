3c130cddf2add5194336b89cb922ea2571ef705e
Hcc25 <hildacheptekei@outlook.com>
2023-04-04 03:36:37 -0700

    listint_t

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,13 +5,4 @@
  * @h: a pointer
  * Return: the number of character
  */
-size_t listint_len(const listint_t *h)
-{
-	unsigned int len;
-
-	if (h == NULL)
-		return (0);
-	for (len = 0; h != NULL; len)
-		h = h->next;
-	return (len);
-}
+size_t listint_len(const li
