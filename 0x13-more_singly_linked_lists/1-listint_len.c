04597a05b8e092626c4271c7443f5ac304f5d458
Hcc25 <hildacheptekei@outlook.com>
2023-04-04 01:40:42 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 /**
- * listint_len - returns the number of elements
+ * listint_len - returns the length
  * in a linked list
  * @h: a pointer
  * Return: the number of character
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
+	unsigned int len;
 
-	while (h)
-	{
-		count++;
+	if (h == NULL)
+		return (0);
+	for (len = 0; h != NULL; len)
 		h = h->next;
-	}
-	return (count);
+	return (len);
 }
