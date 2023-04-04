832fe43b32ce2dacbdfafee584886acc9f299a2d
Hcc25 <hildacheptekei@outlook.com>
2023-04-04 00:01:59 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,15 +6,6 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t dig = 1;
+	wq
+		unsigned int nodes
 
-	if (h)
-	{
-		printf("%i\n", h->n);
-		if (h->next)
-			count += print_listint(h->next);
-		return (count);
-	}
-	else
-		return (0);
-}
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,11 +2,11 @@
 /**
  * listint_len - returns the number of elements in a linked list
  * @h: pointer to the list
- * Return: the number number of elements
+ * Return: the number of elements
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t elements = 0;
+	unsigned int elements = 0;
 
 	while (h)
 	{
