d389998c9ca5292cd939683e73f31066ba35e877
Hcc25 <hildacheptekei@outlook.com>
2023-04-06 04:59:45 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to prit in binay
+ */
+void print_binary(unsigned long int n)
+{
+	int d; count = 0;
+	unsigned long int z;
+
+	for (d = 63; d >= 0; d--)
+	{
+		z = n >> d;
+		if (z & 1)
+		{
+			_putchar('1');
+			z++;
+		}
+		else if (z)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
