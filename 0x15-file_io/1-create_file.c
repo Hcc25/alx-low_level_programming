6d33dbfa4b8df1e5effa571dad0cb018ed361e1f
Hcc25 <hildacheptekei@outlook.com>
2023-05-12 10:36:05 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,19 +9,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int a, b, c;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
-		for (c = 0; text_content[c];)
-			c++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	b = write(b, text_content, c);
-	if (a == -1 || b == -1)
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
-	close(a);
+
+	close(o);
+
 	return (1);
 }
