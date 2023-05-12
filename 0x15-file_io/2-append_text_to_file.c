13b95ff4342f2a1d640d5548e8ac8df6b7d69ff1
Hcc25 <hildacheptekei@outlook.com>
2023-05-12 10:37:58 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,19 +7,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int a, b, c;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
-		for (c = 0; text_content[c];)
-			c++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	a = open(filename, O_WRONLY | O_APPEND);
-	b = write(a, text_content, c);
-	if (a == -1 || b == -1)
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
-	close(a);
+
+	close(o);
+
 	return (1);
 }
